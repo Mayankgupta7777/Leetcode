90f87ec973cba8054d67d294517d0973677e659f
Mayank Gupta <99314506+Mayankgupta7777@users.noreply.github.com>
2022-07-21 14:53:15 +0530

    Create L#92 Reverse Linked List II.py

---
+++ L#92 Reverse Linked List II.py
@@ -0,0 +1,19 @@
+class Solution:
+    def reverseBetween(self, head: Optional[ListNode], left: int, right: int) -> Optional[ListNode]:
+        values = []                                 #to stores the values that needed to be reverse
+        nodes = []                                  #to keep track of the nodes
+        counter = 0
+        curr = head
+        
+        
+        while curr:
+            counter += 1
+            if counter >= left and counter <= right:
+                values.append(curr.val)             #adding the value and nodes in the list
+                nodes.append(curr)                          
+            curr = curr.next
+            
+            
+        for node in nodes:
+            node.val = values.pop(-1)               #for replacing the values of the nodes
+        return head
