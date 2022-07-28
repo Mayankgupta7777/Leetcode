ae425e5c7fac150d30d15ad8126e51aefd13a48c
Mayank Gupta <99314506+Mayankgupta7777@users.noreply.github.com>
2022-07-28 19:54:06 +0530

    Create 6. Zigzag Conversion.py

---
+++ 6. Zigzag Conversion.py
@@ -0,0 +1,18 @@
+class Solution:
+    def convert(self, s: str, numRows: int) -> str:
+        if numRows==1:
+            return s
+        res = [''] * numRows 
+        idx = 0 
+        step = 1 
+        for c in s:
+            res[idx] += c
+            if idx == 0:
+                step = 1
+            elif idx == numRows-1:
+                step = -1
+            idx += step
+        return ''.join(res)
+      
+      
+ #RESULT: 54ms
